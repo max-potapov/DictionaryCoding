89875522c4d6ca2d7d563465284f6a6021f3e209
Alexey Rogatkin <alexey.rogatkin@team.wrike.com>
2021-02-07 19:34:27 +0100

    Remove platforms info

--- Package.swift
+++ Package.swift
@@ -4,22 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "DictionaryCoding",
-    platforms: [
-        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10)
-    ],
     products: [
         .library(
             name: "DictionaryCoding",
             targets: ["DictionaryCoding"]),
     ],
-    dependencies: [
-    ],
     targets: [
         .target(
             name: "DictionaryCoding",
             dependencies: []),
-        .testTarget(
-            name: "DictionaryCodingTests",
-            dependencies: ["DictionaryCoding"]),
     ]
 )
